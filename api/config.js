9ae18e4f07fb8ce7ab3f43141cc75663fd965c6c
Ziwu Liu <liuz0j@Ziwu-macbook3-3.local>
2025-10-04 13:33:28 +0300

    try 1

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 // Vercel Serverless Function
 // 这个文件会被 Vercel 自动识别为 API 端点
 
-export default function handler(req, res) {
+export default function handler(_req, res) {
   // 在服务器端可以访问环境变量
   res.status(200).json({
     repo: "LZWuuu/valorant-updater",
